afeb2158fae6e34f94f38c43155a6bc3bf383f73
Karolina Grzanka <karolinagrzanka1998@gmail.com>
2019-12-01 10:35:41 +0100

    Moved plotting function to separate file

--- storage/analysis.py
+++ storage/analysis.py
@@ -1,29 +1,29 @@
 from datetime import datetime
-from collections import defaultdict
-from operator import attrgetter
 from typing import List, Dict
 
-from storage.warehouse import Warehouse, Product, Operation, OperationType, Sex, Size
-import matplotlib.pyplot as plt
+from storage.warehouse import Warehouse, Product, Operation, OperationType
 
 """ Funkcje stanu magazynu """
 
 
-def stock_statuses_for_general_product(id_prefix: str, wh: Warehouse) -> Dict[Product, int]:
+def stock_statuses_for_general_product(id_prefix: str, wh: Warehouse, time: datetime = None) -> Dict[Product, int]:
     """ Funkcja zwracająca ilość w magazynie dla produktów, których id zaczynających się na wpisaną wartość. """
     return {
-        prod: stock_status_for_product(prod, wh)
+        prod: stock_status_for_product(prod, wh, time=time)
         for prod in get_products_starting_with(id_prefix, wh)
     }
 
 
-def stock_status_for_product(prod: Product, wh: Warehouse) -> int:
+def stock_status_for_product(prod: Product, wh: Warehouse, time: datetime = None) -> int:
     """ Funkcja zwracająca ilość w magazynie podanego produktu. """
+    if time is None:
+        time = datetime.now()
+
     count = 0
     for operation in get_product_operations(prod, wh):
 
         # skip operations in future
-        if operation.date > datetime.now():
+        if operation.date > time:
             continue
 
         if operation.type == OperationType.RESUPPLY:
@@ -49,101 +49,3 @@ def get_products_starting_with(id_prefix: str, wh: Warehouse) -> List[Product]:
         for prod in wh.products.values()
         if prod.id.startswith(id_prefix)
     ]
-
-
-def plot_stock_by_color(id_prefix: str, wh: Warehouse):
-    stock = stock_statuses_for_general_product(id_prefix, wh)
-    sexes = sorted({p.sex for p in stock.keys()}, key=attrgetter('value'))
-    colors = sorted({p.color for p in stock.keys()})
-
-    # create plot
-    fig, axes = plt.subplots(nrows=len(colors), ncols=len(sexes), figsize=(12, 8), sharex='all', sharey='all', squeeze=False)
-
-    # set cols titles
-    for ax, sex in zip(axes[0], sexes):
-        ax.set_title(sex.name)
-
-    # set rows titles
-    for ax, color in zip(axes[:, 0], colors):
-        ax.set_ylabel(color, size='large')
-
-    # plot data
-    for row, color in zip(axes, colors):
-        for ax, sex in zip(row, sexes):
-
-            # collect data
-            data = defaultdict(int)
-            for prod, count in stock.items():
-                if prod.color == color and prod.sex == sex:
-                    data[prod.size.name] += count
-
-            # if there is no data skip
-            if not data:
-                continue
-
-            # plot bars
-            bars = ax.bar(*zip(*data.items()))
-
-            # plot numbers
-            for rect in bars:
-                height = rect.get_height()
-                ax.text(rect.get_x() + rect.get_width() / 2.0, height, '%d' % int(height), ha='center', va='bottom')
-
-            # disable ticks
-            ax.set_yticks([])
-
-    fig.tight_layout()
-    plt.show()
-
-
-def plot_stock_by_size(id_prefix: str, wh: Warehouse):
-    stock = stock_statuses_for_general_product(id_prefix, wh)
-    sexes = sorted({p.sex for p in stock.keys()}, key=attrgetter('value'))
-    sizes = sorted({p.size for p in stock.keys()}, key=attrgetter('value'))
-
-    # create plot
-    fig, axes = plt.subplots(nrows=len(sizes), ncols=len(sexes), figsize=(12, 8), sharex='all', sharey='all', squeeze=False)
-
-    # set cols titles
-    for ax, sex in zip(axes[0], sexes):
-        ax.set_title(sex.name)
-
-    # set rows titles
-    for ax, size in zip(axes[:, 0], sizes):
-        ax.set_ylabel(size.name, size='large')
-
-    # plot data
-    for row, size in zip(axes, sizes):
-        for ax, sex in zip(row, sexes):
-
-            # collect data
-            data = defaultdict(int)
-            for prod, count in stock.items():
-                if prod.size == size and prod.sex == sex:
-                    data[prod.color] += count
-
-            # if there is no data skip
-            if not data:
-                continue
-
-            # plot bars
-            bars = ax.bar(*zip(*data.items()))
-
-            # plot numbers
-            for rect in bars:
-                height = rect.get_height()
-                ax.text(rect.get_x() + rect.get_width() / 2.0, height, '%d' % int(height), ha='center', va='bottom')
-
-            # disable ticks
-            ax.set_yticks([])
-
-    fig.tight_layout()
-    plt.show()
-
-
-def plot_stock_statuses_for_general_product(id_prefix: str, wh: Warehouse):
-    x = stock_statuses_for_general_product(id_prefix, wh).keys()
-    y = stock_statuses_for_general_product(id_prefix, wh).values()
-
-    plt.bar(x, y)
-    plt.show()
---
+++ storage/plots.py
@@ -0,0 +1,108 @@
+from collections import defaultdict
+from datetime import datetime
+from operator import attrgetter
+
+import matplotlib.pyplot as plt
+
+from storage.analysis import stock_statuses_for_general_product
+from storage.warehouse import Warehouse
+
+
+def plot_stock_by_color(id_prefix: str, wh: Warehouse, time: datetime = None):
+    """ Wyswietla wykresy (po kolorach) statusu produktow zaczynajacych sie od podanego id """
+    stock = stock_statuses_for_general_product(id_prefix, wh, time=time)
+    sexes = sorted({p.sex for p in stock.keys()}, key=attrgetter('value'))
+    colors = sorted({p.color for p in stock.keys()})
+
+    # create plot
+    fig, axes = plt.subplots(nrows=len(colors), ncols=len(sexes), figsize=(12, 8), sharex='all', sharey='all', squeeze=False)
+
+    # set cols titles
+    for ax, sex in zip(axes[0], sexes):
+        ax.set_title(sex.name)
+
+    # set rows titles
+    for ax, color in zip(axes[:, 0], colors):
+        ax.set_ylabel(color, size='large')
+
+    # plot data
+    for row, color in zip(axes, colors):
+        for ax, sex in zip(row, sexes):
+
+            # collect data
+            data = defaultdict(int)
+            for prod, count in stock.items():
+                if prod.color == color and prod.sex == sex:
+                    data[prod.size.name] += count
+
+            # if there is no data skip
+            if not data:
+                continue
+
+            # plot bars
+            bars = ax.bar(*zip(*data.items()))
+
+            # plot numbers
+            for rect in bars:
+                height = rect.get_height()
+                ax.text(rect.get_x() + rect.get_width() / 2.0, height, '%d' % int(height), ha='center', va='bottom')
+
+            # disable ticks
+            ax.set_yticks([])
+
+    fig.tight_layout()
+    plt.show()
+
+
+def plot_stock_by_size(id_prefix: str, wh: Warehouse, time: datetime = None):
+    """ Wyswietla wykresy (po rozmiarach) statusu produktow zaczynajacych sie od podanego id """
+    stock = stock_statuses_for_general_product(id_prefix, wh, time=time)
+    sexes = sorted({p.sex for p in stock.keys()}, key=attrgetter('value'))
+    sizes = sorted({p.size for p in stock.keys()}, key=attrgetter('value'))
+
+    # create plot
+    fig, axes = plt.subplots(nrows=len(sizes), ncols=len(sexes), figsize=(12, 8), sharex='all', sharey='all', squeeze=False)
+
+    # set cols titles
+    for ax, sex in zip(axes[0], sexes):
+        ax.set_title(sex.name)
+
+    # set rows titles
+    for ax, size in zip(axes[:, 0], sizes):
+        ax.set_ylabel(size.name, size='large')
+
+    # plot data
+    for row, size in zip(axes, sizes):
+        for ax, sex in zip(row, sexes):
+
+            # collect data
+            data = defaultdict(int)
+            for prod, count in stock.items():
+                if prod.size == size and prod.sex == sex:
+                    data[prod.color] += count
+
+            # if there is no data skip
+            if not data:
+                continue
+
+            # plot bars
+            bars = ax.bar(*zip(*data.items()))
+
+            # plot numbers
+            for rect in bars:
+                height = rect.get_height()
+                ax.text(rect.get_x() + rect.get_width() / 2.0, height, '%d' % int(height), ha='center', va='bottom')
+
+            # disable ticks
+            ax.set_yticks([])
+
+    fig.tight_layout()
+    plt.show()
+
+
+def plot_stock(id_prefix: str, wh: Warehouse, time: datetime = None):
+    """ Wyswietla wykresy (typ wybrany automatycznie) statusu produktow zaczynajacych sie od podanego id """
+    if id_prefix[0] in ['T', 'Q', 'C']:
+        plot_stock_by_size(id_prefix, wh, time)
+    else:
+        plot_stock_by_color(id_prefix, wh, time)
